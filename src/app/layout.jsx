1bfc01395bdd4a2c8ce6a9719b5b5d2e0e6412f8
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-12-15 04:13:21 +0530

    install tailwind css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,21 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Google Clone',
-  description: 'Google Clone Next js',
-}
+  title: "Google Clone",
+  description: "Google Clone Next js",
+  icon: "/favicon.ico",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
